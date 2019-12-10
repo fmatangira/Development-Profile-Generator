e9b5ed71619b0f4dc8b14d87307d99f57b838fad
Farai Matangira <fmatangira@gmail.com>
2019-12-09 23:05:57 -0600

    started working on html

--- index.js
+++ index.js
@@ -11,17 +11,21 @@ getProfile();
 
 async function getProfile() {
 
-
     await inquirer.prompt({
         type: 'input',
         name: 'username',
         message: 'What is your Github username?'
     }).then(function ({ username }) {
         const queryUrl = `https://api.github.com/users/${username}`;
+        const githubUrl = `https://www.github.com/${username}`
 
         axios.get(queryUrl).then(function (data) {
 
-            console.log(data.data);
+            var profile = data.data
+            var githubDetails = [profile.avatar_url,profile.name,profile.company,profile.location,githubUrl,profile.blog,profile.bio,profile.public_repos,profile.followers,profile.public_gists,profile.following];
+            var [profilePic,userName,company,userLocal,githubProfile,userBlog,userBio,publicRepos,followers,userStars,following] = githubDetails;
+            console.log(profilePic,userName,company,userLocal,githubProfile,userBlog,userBio,publicRepos,followers,userStars,following);
+            console.log(profile);
 
         });
 
